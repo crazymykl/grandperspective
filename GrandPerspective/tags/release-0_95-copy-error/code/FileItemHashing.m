70d2dd7a00eb181fb86de77d3c0049f3048d4c9c
erwin <erwin@b82a47f9-25c6-438f-ab64-01e370b75a6b>
2007-02-17 20:23:24 +0000

    Removed two incorrect tag attemps for Version 0.95 (now that is clear what went wrong).

--- GrandPerspective/tags/release-0_95-copy-error/code/FileItemHashing.m
+++
@@ -1,9 +0,0 @@
-#import "FileItemHashing.h"
-
-@implementation FileItemHashing
-
-- (int) hashForFileItem:(FileItem*)item depth:(int)depth {
-  return 0;
-}
-
-@end
--- GrandPerspective/tags/release-0_95-copy-error/code/filter/StringEqualityTest.h
+++
@@ -1,8 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-#import "MultiMatchStringTest.h"
-
-@interface StringEqualityTest : MultiMatchStringTest {
-}
-
-@end
